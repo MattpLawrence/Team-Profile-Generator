6e78e2cfc83f3cc199ee00cb8273384bdf6620c4
Matthew Lawrence <Mattplawrence@gmail.com>
2022-01-20 16:38:56 -0500

    work on respons function

--- index.js
+++ index.js
@@ -8,7 +8,23 @@ inquirer
       message: "Team member's name:",
       name: "name",
     },
+    {
+      type: "list",
+      message: "Team member's role:",
+      name: "role",
+      choices: ["Team Manager", "Engineer", "Intern"],
+    },
   ])
   .then((response) => {
     console.log(response);
   });
+
+let getRole = (response) => {
+  if (response.role === "Team Manager") {
+    console.log(`getRole = Manager`);
+  } else if (response.role === "Engineer") {
+    console.log(`getRole = Engineer`);
+  } else if (response.role === "Intern") {
+    console.log(`getRole = Intern`);
+  }
+};
