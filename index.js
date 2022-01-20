c775b64563be94c95902442ef208e350ac8fe8bc
Matthew Lawrence <Mattplawrence@gmail.com>
2022-01-20 16:14:54 -0500

    add inquirer, adding classes.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+inquirer
+  .prompt([
+    { type: "input", message: "Team member first name:", name: "firstName" },
+    {
+      type: "input",
+      message: "Team member last name:",
+      name: "lastName",
+    },
+  ])
+  .then((response) => {
+    console.log(response);
+  });
