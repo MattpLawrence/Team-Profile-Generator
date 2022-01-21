86424919bafcba91dd622e8dd293e1acda0f4876
Matthew Lawrence <Mattplawrence@gmail.com>
2022-01-21 10:17:42 -0500

    update all

--- index.js
+++ index.js
@@ -1,39 +1,82 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const Employee = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const { get } = require("lodash");
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Team member's name:",
-      name: "name",
-    },
-    {
-      type: "list",
-      message: "Team member's role:",
-      name: "role",
-      choices: ["Team Manager", "Engineer", "Intern"],
-    },
-  ])
-  .then((response) => {
-    let newEmployee = new Employee(response);
-    console.log(response);
-  });
+function firstPrompts() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Team member's role:",
+        name: "role",
+        choices: ["Team Manager", "Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        message: "Team member's name:",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Team member's ID:",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Team member's email:",
+        name: "email",
+      },
+    ])
+    .then((response) => {
+      if (response.role === "Team Manager") {
+        // getManager(response);
+      } else if (response.role === "Engineer") {
+        getEngineer(response);
+      } else if (response.role === "Intern") {
+        getIntern(response);
+      } else {
+        console.log("You must select a role.");
+      }
+    });
+}
+init();
 
-let getRole = (response) => {
-  if (response.role === "Team Manager") {
-    console.log(`getRole = Manager`);
-  } else if (response.role === "Engineer") {
-    console.log(`getRole = Engineer`);
-  } else if (response.role === "Intern") {
-    console.log(`getRole = Intern`);
-  } else {
-    console.log("You must select a role.");
-  }
-};
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Do you want to add a new team member?",
+        name: "init",
+        choices: ["Yes", "No"],
+      },
+    ])
+    .then((response) => {
+      if (response.init === "Yes") {
+        firstPrompts();
+      }
+    });
+}
 
-// let destResponse = (response) => {
-//   name = response.name;
-//   role = response.role;
-// };
+function getEngineer(firstResponse) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Team member's github:",
+        name: "github",
+      },
+    ])
+    .then((engResponse) => {
+      const newEngineer = new Engineer(
+        firstResponse.name,
+        firstResponse.id,
+        firstResponse.email,
+        engResponse.github
+      );
+      console.log(newEngineer);
+      console.log(newEngineer.getRole());
+    });
+}
