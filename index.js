75b036ed946b9b88b8097291fdb86cf9d55ae9ab
Matthew Lawrence <Mattplawrence@gmail.com>
2022-01-24 20:39:15 -0500

    update css and fontawesome

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ function createFinalContent() {
     <title>Team</title>
     <script src = "https://kit.fontawesome.com/c502137733.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/style.css">
   </head>
   <body>
     <div class="container-fluid">
