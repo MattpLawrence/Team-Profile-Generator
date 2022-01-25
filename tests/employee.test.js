124836ddeb6999348989cc1ec9b2b804a98a22dc
Matthew Lawrence <Mattplawrence@gmail.com>
2022-01-24 21:06:15 -0500

    Update employee.test.js

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,12 +1,22 @@
 const Employee = require("../lib/employee");
 
 describe("Employee", () => {
-  describe("Create Employee", () => {
+  describe("Create Employee name", () => {
     it("Should return an object containing a 'name' property.", () => {
-      const obj = new Employee("Matt", "", "");
-      expect("name" in obj).toEqual(true);
-      expect("id" in obj).toEqual(false);
-      expect("email" in obj).toEqual(false);
+      const obj = new Employee("Matt");
+      expect(obj.name).toEqual("Matt");
+    });
+  });
+  describe("Create Employee id", () => {
+    it("Should return an object containing a 'id' property.", () => {
+      const obj = new Employee("", 22, "");
+      expect(obj.id).toEqual(22);
+    });
+  });
+  describe("Create Employee email", () => {
+    it("Should return an object containing a 'email' property.", () => {
+      const obj = new Employee("", "", "myEmail@gmail.com");
+      expect(obj.email).toEqual("myEmail@gmail.com");
     });
   });
 });
